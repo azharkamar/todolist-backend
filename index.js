3c57ba4b078669f166d342a7d85583b3c4199fb2
azharkamar <azhar@azharkamar.com>
2025-04-17 20:12:56 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const taskRoutes = require('./routes/tasks');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/tasks', taskRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => app.listen(5000, () => console.log('Server running on port 5000')))
+  .catch(err => console.log(err));
